731eedd2e60862d0f71d7eb403b72f6e4c565360
Peter Snoek <petersnoek@davinci.nl>
2016-09-12 11:46:34 +0200

    bestand openen. loginform toegevoegd (maar uitgecomment)

---
+++ Kladblok/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Kladblok
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.bestandToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nieuwToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.opslaanToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.afsluitenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bestandToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(414, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // bestandToolStripMenuItem
+            // 
+            this.bestandToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nieuwToolStripMenuItem,
+            this.openenToolStripMenuItem,
+            this.opslaanToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.afsluitenToolStripMenuItem});
+            this.bestandToolStripMenuItem.Name = "bestandToolStripMenuItem";
+            this.bestandToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.bestandToolStripMenuItem.Text = "Bestand";
+            // 
+            // nieuwToolStripMenuItem
+            // 
+            this.nieuwToolStripMenuItem.Name = "nieuwToolStripMenuItem";
+            this.nieuwToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.nieuwToolStripMenuItem.Text = "Nieuw";
+            this.nieuwToolStripMenuItem.Click += new System.EventHandler(this.nieuwToolStripMenuItem_Click);
+            // 
+            // openenToolStripMenuItem
+            // 
+            this.openenToolStripMenuItem.Name = "openenToolStripMenuItem";
+            this.openenToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openenToolStripMenuItem.Text = "Openen..";
+            this.openenToolStripMenuItem.Click += new System.EventHandler(this.openenToolStripMenuItem_Click);
+            // 
+            // opslaanToolStripMenuItem
+            // 
+            this.opslaanToolStripMenuItem.Name = "opslaanToolStripMenuItem";
+            this.opslaanToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.opslaanToolStripMenuItem.Text = "Opslaan";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // afsluitenToolStripMenuItem
+            // 
+            this.afsluitenToolStripMenuItem.Name = "afsluitenToolStripMenuItem";
+            this.afsluitenToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.afsluitenToolStripMenuItem.Text = "Afsluiten";
+            this.afsluitenToolStripMenuItem.Click += new System.EventHandler(this.afsluitenToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(0, 24);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(414, 339);
+            this.textBox1.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Tekstbestanden|*.txt";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 363);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "C# Kladblok";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem bestandToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nieuwToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem opslaanToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem afsluitenToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Kladblok/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kladblok
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            SetTitle(null);
+            //Hide();
+            //LoginForm f = new LoginForm();
+            //DialogResult r = f.ShowDialog();
+            //switch (r)
+            //{
+            //    case DialogResult.Cancel:
+            //        Environment.Exit(0);
+            //        //Application.Exit();
+            //        break;
+            //    case DialogResult.OK:
+            //        var UserName = f.GetUserName();
+            //        this.Show();
+            //        break;
+            //}
+
+        }
+
+        private void afsluitenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // close the application and this form.
+            Application.Exit();
+        }
+
+        private void nieuwToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // maak de textbox leeg
+            textBox1.Clear();
+
+            // stel titel in op default
+            SetTitle(null);
+        }
+
+        private void SetTitle(string newTitle)
+        {
+            if (newTitle == null)
+            {
+                newTitle = "Nieuw document";
+            }
+            this.Text = newTitle + " - C# Kladblok";
+        }
+
+        private void openenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult r = openFileDialog1.ShowDialog();
+
+            if ( r==DialogResult.OK)
+            {
+                try
+                {
+                    textBox1.Text = System.IO.File.ReadAllText(openFileDialog1.FileName);
+                    string OnlyName = System.IO.Path.GetFileNameWithoutExtension(openFileDialog1.FileName);
+                    SetTitle(OnlyName);
+
+                }
+                catch (Exception err)
+                {
+                    MessageBox.Show("Er is iets fout gegaan tijdens het laden van het bestand.", "Fout", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                
+            }
+        }
+    }
+}
